ec19c2c52716710010b408601de63a7f6d5e5078
sduffney <148494340+sduffney@users.noreply.github.com>
2025-11-13 20:10:35 -0500

    Update Player.cs

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -13,4 +13,12 @@ public class Player : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PickUp")) 
+        {
+
+        }
+    }
 }
